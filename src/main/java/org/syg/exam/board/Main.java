ebea32bbdc59956a84ec6f1e60ed32349ef95edd
songyonggi <thddydrl03@gmail.com>
2025-03-19 17:36:16 +0900

    exit를 입력 받기 전까지 명령어 계속 입력

--- src/main/java/org/syg/exam/board/Main.java
+++ src/main/java/org/syg/exam/board/Main.java
@@ -8,11 +8,19 @@ public class Main {
 
            System.out.println("== 게시판 v 0.1 ==");
            System.out.println("== 게시판 시작 ==");
-           System.out.printf("명령)");
-           String cmd = sc.nextLine();
-           System.out.printf("입력된 명령어 : %s\n", cmd);
-           System.out.println("== 게시판 실행 끝 ==");
 
-           sc.close();
+           while(true) {
+               System.out.printf("명령)");
+               String cmd = sc.nextLine();
+               System.out.printf("입력된 명령어 : %s\n", cmd);
+
+               if (cmd.equals("exit")) {
+                   System.out.println("프로그램 종료합니다");
+                   break;
+               }
+           }
+            System.out.println("== 게시판 실행 끝 ==");
+
+            sc.close();
         }
     }
